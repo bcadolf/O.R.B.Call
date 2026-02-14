50bfaf38c12c6f05cabe72a99453d75b2da7f65f
bcadolf <adolfbrandon96@gmail.com>
2026-02-13 20:42:21 -0500

    added in db for beast to unity and confirmed load

--- Assets/Resources/Data/BeastData.cs
+++ Assets/Resources/Data/BeastData.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
+using System;
 
-
+[Serializable]
 public class BeastData
 {
     public List<BeastSingle> Beasts;
 }
 
+[Serializable]
 public class BeastSingle
 {
     public int id;
@@ -15,4 +17,21 @@ public class BeastSingle
     public int energy;
     public List<BeastAbility> abilities;
     public List<LootDrop> lootDrops;
+}
+
+[Serializable]
+public class BeastAbility
+{
+    public string move;
+    public int attack;
+    public int defense;
+    public int bonus;
+    public int energyCost;
+}
+
+[Serializable]
+public class LootDrop
+{
+    public int orbId;
+    public float dropPercent;
 }
--- Assets/Scripts/Characters/NPC/BeastStatsSO.cs
+++ Assets/Scripts/Characters/NPC/BeastStatsSO.cs
@@ -15,20 +15,3 @@ public class BeastStatsSO : ScriptableObject
     public List<BeastAbility> abilities;
     public List<LootDrop> lootDrops;
 }
-
-[System.Serializable]
-public class BeastAbility
-{
-    public string move;
-    public int attack;
-    public int defense;
-    public int bonus;
-    public int energyCost;
-}
-
-[System.Serializable]
-public class LootDrop
-{
-    public int orbId;
-    public float dropPercent;
-}
